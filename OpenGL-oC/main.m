bf9b8c653d7db0ac1d373e333787dd8923e6b216
Max Bilbow <Max@RattleAir.local>
2015-02-12 20:17:20 +0000

    Initial Commit

---
+++ OpenGL-oC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenGL-oC
+//
+//  Created by Max Bilbow on 12/02/2015.
+//  Copyright (c) 2015 Rattle Media Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
